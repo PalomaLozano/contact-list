8338442eacbca3d388cadb197c96664ef565a045
Paloma Lozano <palomalogu9@gmail.com>
2022-01-10 19:02:39 +0100

    added local storage

--- src/js/funciones.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const guardarContact = (db, contacto) => {
-  db.setItem(JSON.stringify(contacto));
-};
--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,12 @@ const listadoTarea = document.querySelector('.listado-tareas');
 
 const db = window.localStorage;
 
+const guardarContacto = (db, contacto) => {
+  db.setItem(contacto.id, JSON.stringify(contacto));
+  //esto refresca la pag cada vez que se pulse el botón, para que no se queden los datos
+  window.location.href = '/';
+};
+
 btnAgregarTarea.onclick = () => {
   let contacto = {
     id: Math.random(1, 100),
@@ -15,4 +21,7 @@ btnAgregarTarea.onclick = () => {
     numero: telefono.value,
     direccion: direccion.value,
   };
+  guardarContacto(db, contacto);
 };
+
+//recoger contactos y mostrarlos
